e9be07c091be32fa2fbe33fcf61d24144fd10431
itggot-markus-moen <markus.moen@itggot.se>
2018-04-18 13:34:20 +0200

    Added negative and even. Also added lib.rb

---
+++ lib/is_negative.rb
@@ -0,0 +1,7 @@
+def is_negative(num)
+    if num < 0
+        return true
+    else 
+        return false
+    end
+end
---
+++ lib/lib.rb
@@ -0,0 +1,7 @@
+require_relative "next_number.rb"
+require_relative "previous_number.rb"
+require_relative "square.rb"
+require_relative "cube.rb"
+
+require_relative "is_negative.rb"
+require_relative "is_even.rb"
