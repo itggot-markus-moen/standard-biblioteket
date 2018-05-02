78b5281898067a39fad409e5325499fe086a3508
itggot-markus-moen <markus.moen@itggot.se>
2018-05-02 13:16:04 +0200

    Added first of.

---
+++ lib/first_of.rb
@@ -0,0 +1,13 @@
+# Public: Takes an array as input and returns the first element in the array.
+#
+# arr - The Array that the output will be taken from.
+#
+# Examples
+#
+#   first_of([1337, 2, -1])
+#   # => 1337
+#
+# Returns the first element in the array.
+def first_of(arr)
+    return arr[0]
+end
