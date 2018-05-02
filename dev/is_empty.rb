f811e881ef8f7dd920ad6da0bfe019e63a5f9706
itggot-markus-moen <markus.moen@itggot.se>
2018-05-02 14:46:09 +0200

    working on is_empty.

---
+++ dev/is_empty.rb
@@ -0,0 +1,10 @@
+# Public: Tells if a string is empty or not.
+#
+# string - The String that might be empty.
+#
+# Examples
+#
+#   is_empty("Tjo!")
+#   # => false
+#
+# Returns if the string is empty or not.
