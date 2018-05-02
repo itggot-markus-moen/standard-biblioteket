dc18ef429fd5288c9c379d75102db141286c7334
itggot-markus-moen <markus.moen@itggot.se>
2018-05-02 13:42:50 +0200

    Added concat.

---
+++ lib/concat.rb
@@ -0,0 +1,14 @@
+# Public: Adds two arrays together
+#
+# arr1 - The Array that will be first.
+# arr2 - The Array that will be last.
+#
+# Examples
+#
+#   concat([1, 2, 3], [4, 5, 6])
+#   # => [1, 2, 3, 4, 5, 6]
+#
+# Returns 
+def concat(arr1, arr2)
+    return arr1 + arr2
+end
